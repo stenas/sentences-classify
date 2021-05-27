c21eb7403b8500bd793d04c0e96592fadf0080ae
Ricardo Pina <naspter@gmail.com>
2021-05-27 14:50:18 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # sentences-classify
+
+Service in  Node JS to classify sentences, only EN.
+## Usage
+Build image, run container, show logs
+```
+docker-compose build && docker-compose up -d && docker-compose logs -f
+```
+## Endpoints
+Classify sentence 
+```
+Method: POST
+Parameter: sentence
+URL: http://[HOST]:[PORT]/sentence
+
+```
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+a
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express')
+require('@tensorflow/tfjs');
+const toxicity = require('@tensorflow-models/toxicity');
+const bodyParser = require('body-parser');
+
+const app = express()
+const port = 8088
+
+// The minimum prediction confidence.
+const threshold = 0.9;
+
+function loadToxicity(req, res, body){
+    toxicity.load(threshold).then(model => {
+        model.classify([body.sentence]).then(predictions => {
+            // `predictions` is an array of objects, one for each prediction head,
+            // that contains the raw probabilities for each input along with the
+            // final prediction in `match` (either `true` or `false`).
+            // If neither prediction exceeds the threshold, `match` is `null`.
+            let hasToxic = false;
+            let toxicSentences = []
+            let message = 'Your sentence contains no toxic words';
+            for(i=0;i<7;i++) {
+                if (predictions[i].results[0].match === true) {
+                    hasToxic = true;
+                    message = 'Your sentence contains toxic words';
+                    toxicSentences.push({'label':predictions[i].label.toUpperCase(),'pecentage':predictions[i].results[0].probabilities[1].toFixed(4)*100+"%"})
+                }
+            }
+
+            res.status(200).json({
+                error:false,
+                message:message,
+                data:toxicSentences
+            })
+
+        });
+    });
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send("<h1>Sentence Classify Service</h1>")
+});
+
+app.post('/sentence', (req, res) => {
+    loadToxicity(req, res, req.body);
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
